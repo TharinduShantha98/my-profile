7288d4a5c39078f0c7121a2b23852f8f3b0cf786
TharinduShantha98 <tharindushantha4@gmail.com>
2022-03-05 00:57:06 -0800

    updated profile

---
+++ otherPages/myprojects/Assignment/SPA/controller/CustomerController.js
@@ -0,0 +1,500 @@
+/*===================================================customer part=============================*/
+// $("#addCustomer").prop('disabled',true);
+$("#customerId").val("C00-100");
+
+
+
+/* ====================== customer Crud Operation ====================*/
+/*-----------------------customer Add part start------ */
+
+$("#addCustomer").click(function () {
+    $("#customerTable>tr").off('click');
+    $("#updateCustomer").off('click');
+    $("#deleteCustomer").off('click');
+
+
+
+
+    let cusId = $("#customerId").val();
+    let cusFName = $("#customerFName").val();
+    let cusLName = $("#customerLName").val();
+    let cusAddress = $("#customerAddress").val();
+    let cusEmail = $("#customerEmail").val();
+    let cusTelNum = $("#customerTelNum").val();
+
+
+
+    let  customer = new  customerDTO();
+    customer.setId(cusId);
+    customer.setFName(cusFName);
+    customer.setLName(cusLName);
+    customer.setAddress(cusAddress);
+    customer.setEmail(cusEmail);
+    customer.setTelNum(cusTelNum);
+    if(confirm("Are you sure, you want to add this customer")){
+
+        customers.push(customer);
+        addCustomerIdForOrderPart();
+        generateCustomerId();
+        clearCustomerTextField();
+
+    }else{
+
+    }
+
+    console.log(customer);
+    addTableRow();
+    generateId();
+    /*-----------------------customer Add part End ------ */
+
+    tableRowStyle();
+
+
+    /*-------------------customer Table Row click part start ------ */
+
+    let rowIndex;
+
+
+
+    customerTableRowClick();
+
+
+
+
+    /*-------------------customer Table Row click part End ------ */
+
+
+
+
+
+    /*-------------------customer update part start ------ */
+
+    $("#updateCustomer").click(function (){
+        //alert("hello");
+        let id  = $("#customerId").val();
+        console.log(id);
+
+        if(confirm("Are you sure, you want to update this customer ")){
+            for(var j = 0; j < customers.length; j++){
+                if(id == customers[j].getId()){
+                    customers[j].setFName($("#customerFName").val());
+                    customers[j].setLName($("#customerLName").val());
+                    customers[j].setAddress($("#customerAddress").val());
+                    customers[j].setEmail($("#customerEmail").val());
+                    customers[j].setTelNum($("#customerTelNum").val());
+                    // console.log("update");
+                }
+
+            }
+            addTableRow();
+            tableRowStyle();
+            clearCustomerTextField();
+            generateCustomerId();
+            customerTableRowClick();
+
+
+        }else {
+
+        }
+
+
+
+
+
+    })
+
+    /*-------------------customer update part End ------ */
+
+
+
+
+
+    /*-------------------customer delete part Start ------ */
+
+
+    $("#deleteCustomer").click(function () {
+
+        if(confirm("Are you sure, you want to delete this customer")){
+            let id  = $("#customerId").val();
+            for(var i = 0; i < customers.length; i++){
+                if(id == customers[i].getId()){
+                    customers.splice(i,1);
+                }
+
+            }
+            addTableRow();
+            tableRowStyle();
+            clearCustomerTextField();
+            generateCustomerId();
+            customerTableRowClick();
+
+        }else{
+
+        }
+
+
+    })
+
+
+
+
+
+
+})
+
+
+
+function customerTableRowClick(){
+
+    $("#customerTable>tr").click(function (){
+        if(confirm("Are you sure, you want to see this row")){
+            let cusId = $(this).children().eq(0).text();
+            for(let i =0; i < customers.length; i++){
+                if(cusId == customers[i].getId()){
+                    $("#customerId").val(customers[i].getId());
+                    $("#customerFName").val(customers[i].getFName());
+                    $("#customerLName").val(customers[i].getLName());
+                    $("#customerAddress").val(customers[i].getAddress());
+                    $("#customerEmail").val(customers[i].getEmail());
+                    $("#customerTelNum").val(customers[i].getTelNum());
+
+                }
+            }
+            rowIndex  = this.rowIndex;
+            // console.log("rowIndex " + rowIndex);
+
+
+
+        }else {
+
+        }
+
+
+
+    })
+
+
+
+
+}
+
+
+function addTableRow(){
+    $("#customerTable").empty();
+    for (var i =0; i < customers.length; i++){
+        let newRow = `<tr><td>${customers[i].getId()}</td></td><td>${customers[i].getFName()}</td><td>${customers[i].getLName()}
+                </td><td>${customers[i].getAddress()}</td><td>${customers[i].getEmail()}</td><td>${customers[i].getTelNum()}</td></tr>`;
+        $("#customerTable").append(newRow);
+    }
+
+
+}
+
+function tableRowStyle(){
+    $("#customerTable>tr").css("background-color", "#54a0ff");
+
+    let table = document.getElementById("customerTable");
+    for ( var i =0; i< table.rows.length ; i++  ){
+        $("#customerTable").children().eq(i).css("font-weight","bold");
+
+    }
+
+    $("#customerTable>tr").hover(function () {
+        $(this).css("background-color","#95a5a6" );
+    },function () {
+        $(this).css("background-color", "#54a0ff");
+    })
+}
+
+
+
+
+
+function generateId(){
+
+    let index = customers.length;
+    let gId  = customers[index-1].getId();
+    let text =  gId.substr(4,7);
+    let number =  Number(text);
+    let nextIdNum =  number+ 1;
+    let nextId =  "C00-"+nextIdNum;
+    $("#customerId").val(nextId);
+
+}
+
+
+$("#CustomerSearch").keyup(function (event) {
+
+    if(event.keyCode == 13){
+        $("#btnCustomerSearch").focus();
+    }
+
+})
+
+
+$("#btnCustomerSearch").click(function () {
+    let searchItemCode = $("#CustomerSearch").val();
+    for(var k=0; k < customers.length; k++){
+        if(customers[k].getId() == searchItemCode){
+            $("#customerId").val(customers[k].getId());
+            $("#customerFName").val(customers[k].getFName());
+            $("#customerLName").val(customers[k].getLName());
+            $("#customerAddress").val(customers[k].getAddress());
+            $("#cusEmail").val(customers[k].getEmail());
+            $("#customerTelNum").val(customers[k].getTelNum());
+
+        }else{
+            /* alert("this item code not in dataBase");*/
+        }
+
+    }
+
+})
+
+
+
+$("#clearTextCustomer").click(function () {
+    generateCustomerId();
+    clearCustomerTextField();
+})
+
+
+function generateCustomerId(){
+
+    let index = customers.length;
+    let gId  = customers[index-1].getId();
+    let text =  gId.substr(4,7);
+    let number =  Number(text);
+    let nextIdNum =  number+ 1;
+    let nextId =  "C00-"+nextIdNum;
+    $("#customerId").val(nextId);
+
+}
+
+
+
+
+
+function clearCustomerTextField(){
+    //$("#itemCode").val("");
+    //  $("#customerId").val("");
+    $("#customerFName").val("");
+    $("#customerLName").val("");
+    $("#customerAddress").val("");
+    $("#cusEmail").val("");
+    $("#customerTelNum").val("");
+    $("#customerEmail").val("");
+
+
+
+}
+
+
+
+
+
+
+
+/* -------------------customer validation part start------------------*/
+
+
+
+/*  let customerIdRegx = /^(C00)[-][0-9]{3,9}$/
+  let cusFNameRegx = /^[A-z]{3,15}$/;
+  let cusLNameRegx = /^[A-z]{3,15}$/;
+  let cusAddressRegx = /^(No-)[1-9]{1,9}(\s)[A-z]{1,15}|[A-z]{0,20}$/;
+  let cusEmailRegx = /^[A-z0-9$#^&*]{0,30}(@)(gmail.com)$/;
+  let cusTelNumRegx = /^[0-9]{10}$/;
+
+
+
+  function validationCustomer(testRegex, event,id,nextId,correctRegex){
+
+      let test = correctRegex.test(testRegex);
+      if(test){
+          $(id).css("border", "1px solid  green");
+          $(id).css("box-shadow", "0px 0px 10px #5ad25a");
+
+          if(event.keyCode == 13){
+              $(nextId).focus();
+          }
+
+          return true;
+      }else{
+          $(id).css("border", "1px solid  red");
+          $(id).css("box-shadow", "0px 0px 10px #d25a72");
+          $("#addItem").prop('disabled',true);
+          return false;
+      }
+
+
+  }
+
+
+  $("#customerId").keyup(function (event) {
+
+      let code = $("#customerId").val();
+      let boolean = validationCustomer(code,event,"#customerId","#customerFName",customerIdRegx);
+
+      if(boolean){
+          $("#validationC6").text("");
+
+
+      }else{
+          $("#validationC6").text("input format(C00-***)");
+          $("#validationC6").css("color", "red");
+          $("#validationC6").css("font-weight", "bold");
+
+      }
+
+      checkButtonCustomerAdd();
+  })
+
+
+
+  $("#customerFName").keyup(function (event) {
+
+      let code = $("#customerFName").val();
+      let boolean = validationCustomer(code,event,"#customerFName","#customerLName",cusFNameRegx);
+
+      if(boolean){
+          $("#validationC1").text("");
+
+
+      }else{
+          $("#validationC1").text("by A to z characters only one word");
+          $("#validationC1").css("color", "red");
+          $("#validationC1").css("font-weight", "bold");
+
+      }
+
+      checkButtonCustomerAdd();
+  })
+
+  $("#customerLName").keyup(function (event) {
+
+      let code = $("#customerLName").val();
+      let boolean = validationCustomer(code,event,"#customerLName","#customerAddress",cusLNameRegx);
+
+      if(boolean){
+          $("#validationC2").text("");
+
+
+      }else{
+          $("#validationC2").text("by A to z characters only one word");
+          $("#validationC2").css("color", "red");
+          $("#validationC2").css("font-weight", "bold");
+
+      }
+
+      checkButtonCustomerAdd();
+  })
+
+  $("#customerAddress").keyup(function (event) {
+      let code = $("#customerAddress").val();
+      let boolean = validationCustomer(code,event,"#customerAddress","#customerEmail",cusAddressRegx);
+
+      if(boolean){
+          $("#validationC3").text("");
+
+      }else{
+          $("#validationC3").text(" as a no-1 ********");
+          $("#validationC3").css("color", "red");
+          $("#validationC3").css("font-weight", "bold");
+
+      }
+
+      checkButtonCustomerAdd();
+
+  })
+  $("#customerEmail").keyup(function (event) {
+
+      let code = $("#customerEmail").val();
+      let boolean = validationCustomer(code,event,"#customerEmail","#customerTelNum",cusEmailRegx);
+
+      if(boolean){
+          $("#validationC4").text("");
+
+
+      }else{
+          $("#validationC4").text("please input last text as a @gmail.com");
+          $("#validationC4").css("color", "red");
+          $("#validationC4").css("font-weight", "bold");
+
+      }
+
+      checkButtonCustomerAdd();
+  })
+  $("#customerTelNum").keyup(function (event) {
+
+      let code = $("#customerTelNum").val();
+      let boolean = validationCustomer(code,event,"#customerTelNum","#addCustomer",cusTelNumRegx);
+
+      if(boolean){
+          $("#validationC5").text("");
+          $("#addCustomer").prop('disabled', false);
+
+      }else{
+          $("#validationC5").text("please input only numbers");
+          $("#validationC5").css("color", "red");
+          $("#validationC5").css("font-weight", "bold");
+
+      }
+
+
+      checkButtonCustomerAdd();
+  })
+
+
+  /!*function checkButtonCustomerAdd() {
+      if(($("#CustomerFName").val() != "") &&  ($("#customerLName").val() != "") &&  ($("#customerAddress").val() != "") &&
+          ($("#customerEmail").val() != "") &&  ($("#customerTelNum").val() != "")){
+          console.log("false");
+          $("#addCustomer").prop('disabled',false);
+
+      }else {
+          $("#addCustomer").prop('disabled',true);
+          console.log("true");
+      }
+  }*!/
+
+
+
+  function checkButtonCustomerAdd() {
+      if(($("#validationC1").text() == "") &&  ($("#validationC2").text() == "") &&  ($("#validationC3").text() == "") &&
+          ($("#validationC4").text() == "") &&  ($("#validationC5").text() == "") && ($("#validationC6").text() == "") ){
+          console.log("false");
+          $("#addCustomer").prop('disabled',false);
+
+      }else {
+          $("#addCustomer").prop('disabled',true);
+          console.log("true");
+      }
+  }*/
+
+/* -------------------customer validation part end------------------*/
+
+var timeId;
+var position = 1;
+var test =  $("#customerManage").text();
+
+console.log(test);
+function customerManegeFun() {
+
+    var output =test.substr(0,position);
+    position++;
+
+
+    $("#customerManage").text(output);
+
+    if(position == test.length){
+        position = 1;
+    }
+
+
+}
+
+
+timeId = setInterval(customerManegeFun,100);
+/* console.log(timeId);*/
+
---
+++ otherPages/myprojects/Assignment/SPA/controller/ItemController.js
@@ -0,0 +1,414 @@
+/*==============item part =======================*/
+// $("#addItem").prop('disabled',true);
+$("#itemCode").val("I00-100");
+
+
+$("#addItem").click(function () {
+    $("#itemTable>tr").off('click');
+    $("#updateItem").off('click');
+    /*------------------------ add to data for table---------------- */
+
+    let itemCode = $("#itemCode").val();
+    let itemName = $("#itemName").val();
+    let itemUnitPrice = $("#itemUnitPrice").val();
+    let itemPackSize = $("#itemPackSize").val();
+    let itemBuyingPrice = $("#itemBuyingPrice").val();
+    let itemQty = $("#itemQty").val();
+
+
+    var item =  new  ItemDTO();
+    item.setId(itemCode);
+    item.setItemName(itemName);
+    item.setUnitPrice(itemUnitPrice);
+    item.setPackSize(itemPackSize);
+    item.setBuyingPrice(itemBuyingPrice);
+    item.setQuantity(itemQty);
+
+    if(confirm("Are you sure you want to add this item?")){
+        items.push(item);
+        addRowForItemTable();
+        itemTableStyle();
+        clearTextField();
+        addItemIdForOrderPart();
+        generateItemId();
+    }else{
+
+    }
+    /*------------------------------- end add to data for table---------------- */
+
+
+
+    /*-------------------------------------table row click  --------------------------*/
+
+
+
+    itemTableRowClick();
+
+    /*-------------------------------------table row click --------------------------*/
+
+
+
+
+
+
+    /*-------------------------------------update table row click --------------------------*/
+    $("#updateItem").click(function () {
+        let itemId  = $("#itemCode").val();
+        console.log(itemId);
+
+        if(confirm("Are you sure, you want to update this customer?")){
+            for(var i =0; i < items.length; i++){
+                if(itemId == items[i].getId()){
+
+                    items[i].setItemName($("#itemName").val());
+                    items[i].setUnitPrice($("#itemUnitPrice").val());
+                    items[i].setPackSize($("#itemPackSize").val());
+                    items[i].setBuyingPrice($("#itemBuyingPrice").val());
+                    items[i].setQuantity($("#itemQty").val());
+                }
+            }
+        }else{
+
+
+        }
+        addRowForItemTable();
+        itemTableRowClick();
+        itemTableStyle();
+        clearTextField();
+        generateItemId();
+
+
+    })
+
+})
+
+
+
+function itemTableRowClick(){
+    $("#itemTable>tr").click(function () {
+        if(confirm("Are you sure you want to get this item")){
+            let code = $(this).children().eq(0).text();
+            let  name = $(this).children().eq(1).text();
+            let unitPrice = $(this).children().eq(2).text();
+            let packSize = $(this).children().eq(3).text();
+            let buyingPrice = $(this).children().eq(4).text();
+            let qty = $(this).children().eq(5).text();
+
+            $("#itemCode").val(code);
+            $("#itemName").val(name);
+            $("#itemUnitPrice").val(unitPrice);
+            $("#itemPackSize").val(packSize);
+            $("#itemBuyingPrice").val(buyingPrice);
+            $("#itemQty").val(qty);
+        }else{
+
+
+        }
+
+    })
+
+}
+
+
+function addRowForItemTable(){
+    $("#itemTable").empty();
+    for(var i =0; i < items.length; i++){
+        let row = `<tr><td>${items[i].getId()}</td><td>${items[i].getItemName()}</td><td>${items[i].getUnitPrice()}</td><td>${items[i].getPackSize()}</td><td>${items[i].getBuyingPrice()}</td>
+                <td>${items[i].getQuantity()}</td></tr>`;
+        $("#itemTable").append(row);
+    }
+}
+
+
+function itemTableStyle(){
+    let table = document.getElementById("itemTable");
+    for ( var i =0; i< table.rows.length ; i++){
+        $("#itemTable").children().eq(i).css("font-weight","bold");
+        $("#itemTable").children().eq(i).css("text-align","center");
+
+    }
+
+    /*  -------------table row hover ---------------*/
+    $("#itemTable>tr").hover(function () {
+        $(this).css("background-color","#95a5a6" );
+    },function () {
+        $(this).css("background-color", "#54a0ff");
+    })
+
+    $("#itemTable>tr").css("background-color","#54a0ff");
+
+}
+
+
+
+
+/* -------------------------------delete Item ------------------------------*/
+
+$("#deleteItem").click(function () {
+    if(confirm("Are you sure, you want delete this item?")){
+        let id =  $("#itemCode").val();
+        for(let i =0; i <items.length; i++){
+            if(id == items[i].getId()){
+                items.splice(i,1);
+            }else{
+
+            }
+
+        }
+        addRowForItemTable();
+        itemTableStyle();
+        itemTableRowClick();
+        clearTextField();
+        generateItemId();
+
+    }else{
+
+    }
+
+
+
+
+})
+
+
+
+
+
+/*-------------------------------------Search Item--------------------------*/
+
+$("#itemSearch").keyup(function (event) {
+
+    if(event.keyCode == 13){
+        $("#btnItemSearch").focus();
+    }
+
+})
+
+
+
+
+$("#btnItemSearch").click(function () {
+    let searchItemCode = $("#itemSearch").val();
+    for(var k=0; k < items.length; k++){
+        if(items[k].getId() == searchItemCode){
+            $("#itemCode").val(items[k].getId());
+            $("#itemName").val(items[k].getItemName());
+            $("#itemUnitPrice").val(items[k].getUnitPrice());
+            $("#itemPackSize").val(items[k].getPackSize());
+            $("#itemBuyingPrice").val(items[k].getBuyingPrice());
+            $("#itemQty").val(items[k].getQuantity());
+
+        }else{
+
+        }
+
+    }
+
+})
+
+$("#clearText").click(function () {
+    clearTextField();
+    generateItemId();
+})
+
+
+function clearTextField(){
+    //$("#itemCode").val("");
+    $("#itemName").val("");
+    $("#itemUnitPrice").val("");
+    $("#itemPackSize").val("");
+    $("#itemBuyingPrice").val("");
+    $("#itemQty").val("");
+
+
+
+}
+
+function generateItemId(){
+
+    let index = items.length;
+    let gId  = items[index-1].getId();
+    let text =  gId.substr(4,7);
+    let number =  Number(text);
+    let nextIdNum =  number+ 1;
+    let nextId =  "I00-"+nextIdNum;
+    $("#itemCode").val(nextId);
+
+}
+
+
+
+
+
+
+/*-------------text field focus  and validation part of item ------------*/
+let itemIdRegx = /^(I00)[-][0-9]{3,9}$/;
+let itemNameRegx = /^[A-z]{3,15}$/;
+let itemUnitPriceRegx = /^[0-9]{1,9}$/;
+let itemPackSizeRegx = /^[0-9]+(l|g|kg|ml)$/;
+let itemBuyingPriceRegx = /^[0-9]{1,9}$/;
+let itemQtyRegx = /^[0-9]{1,9}$/;
+
+/*
+
+    $('#itemCode,#itemName,#itemUnitPrice,#itemPackSize,#itemBuyingPrice,#itemQty').keydown(function (eventOb) {
+        if (eventOb.key == "Tab") {
+           eventOb.preventDefault();
+
+        }
+    })
+
+
+
+
+    function validation(testRegex, event,id,nextId,correctRegex){
+
+        let test = correctRegex.test(testRegex);
+        if(test){
+            $(id).css("border", "1px solid  green");
+            $(id).css("box-shadow", "0px 0px 10px #5ad25a");
+
+            if(event.keyCode == 13){
+                $(nextId).focus();
+            }
+
+            return true;
+        }else{
+            $(id).css("border", "1px solid  red");
+            $(id).css("box-shadow", "0px 0px 10px #d25a72");
+            $("#addItem").prop('disabled',true);
+            return false;
+        }
+
+
+    }
+
+    $("#itemCode").keyup(function (event) {
+        let code = $("#itemCode").val();
+        let boolean = validation(code,event,"#itemCode","#itemName",itemIdRegx);
+        console.log(boolean);
+
+        if(boolean){
+            $("#validationI1").text("");
+            checkButton();
+        }else{
+            $("#validationI1").text("Input format-(I00-000)")
+            $("#validationI1").css("color", "red");
+            $("#validationI1").css("font-weight", "bold");
+        }
+
+    })
+
+
+    $("#itemName").keyup(function (event) {
+        let code = $("#itemName").val();
+        let boolean = validation(code,event,"#itemName","#itemUnitPrice",itemNameRegx);
+
+        if(boolean){
+            $("#validationI2").text("");
+            checkButton();
+        }else{
+            $("#validationI2").text("by A to z characters only one word")
+            $("#validationI2").css("color", "red");
+            $("#validationI2").css("font-weight", "bold");
+        }
+    })
+
+    $("#itemUnitPrice").keyup(function (event) {
+        let code = $("#itemUnitPrice").val();
+        let boolean = validation(code,event,"#itemUnitPrice","#itemPackSize",itemUnitPriceRegx);
+
+        if(boolean){
+            $("#validationI3").text("");
+            checkButton();
+        }else{
+            $("#validationI3").text("only numbers")
+            $("#validationI3").css("color", "red");
+            $("#validationI3").css("font-weight", "bold");
+        }
+
+
+    })
+
+    $("#itemPackSize").keyup(function (event) {
+        let code = $("#itemPackSize").val();
+        let boolean = validation(code,event,"#itemPackSize","#itemBuyingPrice",itemPackSizeRegx);
+
+        if(boolean){
+            $("#validationI4").text("");
+            checkButton();
+        }else{
+            $("#validationI4").text("please input pack Size (kg/l/ml/l) end of number")
+            $("#validationI4").css("color", "red");
+            $("#validationI4").css("font-weight", "bold");
+        }
+
+    })
+
+    $("#itemBuyingPrice").keyup(function (event) {
+        let code = $("#itemBuyingPrice").val();
+        let boolean = validation(code,event,"#itemBuyingPrice","#itemQty",itemBuyingPriceRegx);
+
+        if(boolean){
+            $("#validationI5").text("");
+            checkButton();
+        }else{
+            $("#validationI5").text("only number")
+            $("#validationI5").css("color", "red");
+            $("#validationI5").css("font-weight", "bold");
+        }
+    })
+
+
+    $("#itemQty").keyup(function (event) {
+        let code = $("#itemQty").val();
+        let boolean = validation(code,event,"#itemQty","#addItem",itemQtyRegx);
+
+        if(boolean){
+            $("#validationI6").text("");
+          /!*  $("#addItem").prop('disabled',false);*!/
+            checkButton();
+        }else{
+            $("#validationI6").text("only number")
+            $("#validationI6").css("color", "red");
+            $("#validationI6").css("font-weight", "bold");
+        }
+
+
+
+
+    })
+
+    function checkButton() {
+        if(($("#itemCode").val() != "") &&  ($("#ItemName").val() != "") &&  ($("#itemUnitPrice").val() != "") &&
+            ($("#itemPackSize").val() != "") &&  ($("#itemBuyingPrice").val() != "") && ($("#itemQty").val() != "")){
+
+            $("#addItem").prop('disabled',false);
+
+
+        }else {
+            $("#addItem").prop('disabled',true);
+
+        }
+    }
+*/
+
+
+var position2 = 1;
+var text = $("#itemManage").text();
+function itemManageFun(){
+    let outPut2 = text.substr(0,position2);
+    $("#itemManage").text(outPut2);
+    position2++;
+
+    if(position2 == text.length){
+        position2 = 1;
+    }
+
+
+}
+
+
+setInterval(itemManageFun,100);
+
+
